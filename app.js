6ab42a55c0b43d9db5186871bb07e2fca6954489
kenoteb <kenoteb@onid.oregonstate.edu>
2017-09-12 15:06:23 -0700

    cors

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 
+const cors = require('cors');
+app.use(cors());
+
 const AuthController = require('./auth/AuthController');
 app.use('/api/auth', AuthController);
 
