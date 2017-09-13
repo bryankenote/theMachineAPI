e1289e74850e1fb51a5c5ea02c44110e3a03b63f
kenoteb <kenoteb@onid.oregonstate.edu>
2017-09-13 13:58:39 -0700

    use /api for root of models

--- app.js
+++ app.js
@@ -9,9 +9,18 @@ const AuthController = require('./auth/AuthController');
 app.use('/api/auth', AuthController);
 
 const UserController = require('./model/user/UserController');
-app.use('/users', UserController);
+app.use('/api/users', UserController);
 
 const MemberController = require('./model/member/MemberController');
-app.use('/members', MemberController);
+app.use('/api/members', MemberController);
+
+const BankController = require('./model/bank/BankController');
+app.use('/api/banks', BankController);
+
+const JobController = require('./model/job/JobController');
+app.use('/api/jobs', JobController);
+
+const FineController = require('./model/fine/FineController');
+app.use('/api/fines', FineController);
 
 module.exports = app;
